01abc92c9474ae592ed1f9ea4aed70477d20cf89
kartavya dave <kdvlogs78@gmail.com>
2025-12-31 13:16:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -244,4 +244,15 @@ function rotate() {
 }
 
 rotate();
+<script>
+function toggleMenu() {
+  const menu = document.getElementById("navMenu");
+
+  if (menu.style.display === "flex") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "flex";
+  }
+}
+</script>
 
